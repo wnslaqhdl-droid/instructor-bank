0d4eef22a527a3418fba6a43cdb35b100ea114fb
wnslaqhdl-droid <wnslaqhdl@gmail.com>
2026-05-18 13:08:57 +0900

    Create useRegisterForm.js

---
+++ src/hooks/useRegisterForm.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { submitInstructorForm } from "../utils/submitInstructorForm";
+
+export default function useRegisterForm({
+  clone,
+  emptyInstructor,
+  emptyTraining,
+  emptyWelfare,
+  emptyLecture,
+  supabase,
+  registerInstructor
+}) {
+
+  const [form, setForm] =
+    useState(clone(emptyInstructor));
+
+  const [trainingCourses,
+    setTrainingCourses] =
+    useState([clone(emptyTraining)]);
+
+  const [welfareExperiences,
+    setWelfareExperiences] =
+    useState([clone(emptyWelfare)]);
+
+  const [lectureExperiences,
+    setLectureExperiences] =
+    useState([clone(emptyLecture)]);
+
+  const [message, setMessage] =
+    useState("");
+
+  const [error, setError] =
+    useState("");
+
+  const [password, setPassword] =
+    useState("");
+
+  const update = (key, value) =>
+    setForm((current) => ({
+      ...current,
+      [key]: value
+    }));
+
+  async function submitForm() {
+
+    await submitInstructorForm({
+      form,
+      password,
+      trainingCourses,
+      welfareExperiences,
+      lectureExperiences,
+      supabase,
+      registerInstructor,
+      setError,
+      setMessage,
+      setForm,
+      setTrainingCourses,
+      setWelfareExperiences,
+      setLectureExperiences,
+      emptyInstructor,
+      emptyTraining,
+      emptyWelfare,
+      emptyLecture,
+      clone
+    });
+
+  }
+
+  return {
+    form,
+    setForm,
+
+    trainingCourses,
+    setTrainingCourses,
+
+    welfareExperiences,
+    setWelfareExperiences,
+
+    lectureExperiences,
+    setLectureExperiences,
+
+    message,
+    error,
+
+    password,
+    setPassword,
+
+    update,
+    submitForm
+  };
+}
