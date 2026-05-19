fb1416a6051e3626b97729afe340ba28f8ee007b
wnslaqhdl-droid <wnslaqhdl@gmail.com>
2026-05-19 14:36:44 +0900

    Update PageSizeSelector.jsx

--- src/components/PageSizeSelector.jsx
+++ src/components/PageSizeSelector.jsx
@@ -1,38 +1,31 @@
 export default function PageSizeSelector({
   value,
-  onChange
+  onChange,
+  label = "목록 개수"
 }) {
+
   return (
     <div
       style={{
         display: "flex",
         alignItems: "center",
-        gap: "8px"
+        gap: "6px"
       }}
     >
-      <span>목록 개수</span>
+      <span className="small muted">
+        {label}
+      </span>
 
       <select
         value={value}
-        onChange={(e) =>
+        onChange={(e)=>
           onChange(Number(e.target.value))
         }
       >
-        <option value={10}>
-          10개
-        </option>
-
-        <option value={20}>
-          20개
-        </option>
-
-        <option value={50}>
-          50개
-        </option>
-
-        <option value={100}>
-          100개
-        </option>
+        <option value={10}>10개</option>
+        <option value={20}>20개</option>
+        <option value={50}>50개</option>
+        <option value={100}>100개</option>
       </select>
     </div>
   );
