5b6c933db15a4c41aab3e1fab816c53d46128e9f
wnslaqhdl-droid <wnslaqhdl@gmail.com>
2026-05-21 09:15:51 +0900

    Update ModifyWelfareSection.jsx

--- src/components/ModifyWelfareSection.jsx
+++ src/components/ModifyWelfareSection.jsx
@@ -49,7 +49,6 @@ export default function ModifyWelfareSection({
                     setModifyWelfares
                   )
                 }
-                }
               />
             </Field>
 
