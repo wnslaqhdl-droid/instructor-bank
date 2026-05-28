fd40f67e7eab1642f340b0671aa0747283bd6200
wnslaqhdl-droid <wnslaqhdl@gmail.com>
2026-05-28 20:12:56 +0900

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const regionOptions = ["서울","부산","대구","인천","광주","대전","울산","세종","경기","강원","충북","충남","전북","전남","경북","경남","제주"];
 export const targetOptions = ["발달장애인 당사자","부모·보호자","가족","관련 종사자"];
 export const typeOptions = ["상담형","강의형","토론형","체험형"];
-export const specialtyOptions = ["인간 성의 특성","발달장애인의 장애 특성과 성적 발달","발달장애인의 사회적 능력과 대인관계","가정(시설) 내 성교육","성폭력 예방교육"];
+export const specialtyOptions = ["인간 성의 특성","발달장애인의 장애 특성과 성적 발달","발달장애인의 사회적 능력과 대인관계","가정(시설) 내 성교육","성폭력 예방교육","교육조건부 기소유예"];
 export const emptyInstructor = {
   name:"",
   phone:"",
